0a7f39cf314209dd403ada9be0376247a2963f26
Ishita05A <ishita05agarwal@gmail.com>
2025-05-27 20:19:22 +0530

    updated code

--- Basic/P1.py
+++ Basic/P1.py
@@ -1,2 +1,5 @@
 print("Hello World 😊")
 print("😎 "*10)
+x = 1
+y = 2
+unit_price = 3
---
+++ Basic/String.py
@@ -0,0 +1,6 @@
+course = "Python Programming"
+print(len(course))
+print(course[2:8])
+print(course[0:])
+print(course[:])
+print(course[-1])
