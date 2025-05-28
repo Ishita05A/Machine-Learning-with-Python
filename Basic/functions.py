0a0e6995593e8aeb5a2dd982fcb1b08b8192ba6a
Ishita05A <ishita05agarwal@gmail.com>
2025-05-28 21:39:34 +0530

    updated code

--- Basic/functions.py
+++ Basic/functions.py
@@ -3,5 +3,20 @@ def greet(First_name, Last_name):
     print("Welcome Abroad")
 
 
+def increment(number, by=1):
+    return number+by
+
+
+def multiply(*num):
+    total = 1
+    for i in num:
+        total *= i
+    return total
+
+
 greet("Ishita", "Agarwal")
 greet("Harshita", "Pandey")
+
+print(increment(5, 2))
+
+print(multiply(3, 4, 5, 2, 1))
