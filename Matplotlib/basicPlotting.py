55a324ca8ca5e402a84e681308f21c9c607f5212
Ishita05A <ishita05agarwal@gmail.com>
2025-06-13 01:30:28 +0530

    updated code

---
+++ Matplotlib/basicPlotting.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+x = (1, 2, 3)
+y = (2, 4, 6)
+plt.title('My First graph')
+plt.plot(x, y)
+plt.show()
+a = (3, 5, 2)
+b = (7, 8, 3)
+plt.title('My Second Graph')
+plt.plot(a, b)
+plt.show()
