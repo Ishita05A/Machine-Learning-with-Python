01bcb26dca9fc0c7a2e239df389778666464b5a8
Ishita05A <ishita05agarwal@gmail.com>
2025-06-17 00:48:06 +0530

    updated code

--- Matplotlib/Histogram.py
+++ Matplotlib/Histogram.py
@@ -4,5 +4,10 @@ fifa = pd.read_csv('./Matplotlib/fifa_data.csv')
 num_rows = 5
 num_cols = 5
 print(fifa.iloc[:num_rows, :num_cols])
-plt.hist(fifa.Overall)
+bins = [40, 50, 60, 70, 80, 90, 100]
+plt.xticks(bins)
+plt.title('Distribution of player skill in FIFA 2018')
+plt.xlabel('Skill Level')
+plt.ylabel('Number of Players')
+plt.hist(fifa.Overall, bins, color="#18497a")
 plt.show()
