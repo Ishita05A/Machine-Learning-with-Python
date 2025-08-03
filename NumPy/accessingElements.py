92639fb2451ed0e76b8237a80645ebb304981d40
Ishita05A <ishita05agarwal@gmail.com>
2025-08-03 11:19:16 +0530

    updated code

--- NumPy/accessingElements.py
+++ NumPy/accessingElements.py
@@ -3,8 +3,11 @@ a = np.array([[1, 2, 3, 4, 5, 6], [3, 4, 5, 6, 7, 4]])
 print(a[1, 4])
 print(a[0, :])
 print(a[:, 2])
+print()
 print(a[0, 1: 5: 2])
 a[1, 2] = 45
+print()
 print(a)
+print()
 a[:, 3] = [1, 2]
 print(a)
